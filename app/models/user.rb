c79c3af7f99fba3f13e8ac24588182d5f117fa83
mr-jungchoi <mr.jungchoi@gmail.com>
2016-11-25 13:37:17 -0500

    Fix uniqueness typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, uniqueness: true, presence: true
-  validates :email, presence: true, unique: true
+  validates :email, presence: true, uniqueness: true
   has_secure_password
   has_many :votes
   has_many :comments
