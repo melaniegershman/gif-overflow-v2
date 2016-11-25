dffeda24546ec3d95c19212f146e9d3b9739c047
Jenna Van Conett <jenvanc@gmail.com>
2016-11-25 10:48:32 -0500

    Update 20161124211640_create_comments.rb

--- db/migrate/20161124211640_create_comments.rb
+++ db/migrate/20161124211640_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :body, null: false
       t.references :user, null: false
-      t.integer :commentable, polymporphic: true, index: true
+      t.integer :commentable, polymorphic: true, index: true
       
       t.timestamps(null: false)
     end
