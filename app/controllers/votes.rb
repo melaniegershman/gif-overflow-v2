8f9df0c6aec38b616083009500014a2bf8ad6309
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2016-11-25 17:02:05 -0500

    Create voting functionality

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -4,7 +4,7 @@ get '/questions/:question_id/votes/new' do
   erb :'votes/_vote_form'
 end
 
-post ':voteable_type/:voteable_id/votes' do
+post '/votes' do
   Vote.create(user: current_user, voteable_id: params[:voteable_id], voteable_type: convert_to_class_name(params[:voteable_type]))
-  
+
 end
