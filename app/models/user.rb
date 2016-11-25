0a65b06e15f0354a627b9e4589769826abee7a6a
Jenna Van Conett <jenvanc@gmail.com>
2016-11-25 10:45:42 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, unique: true, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, unique: true
   has_secure_password
   has_many :votes
   has_many :comments
