67c7779d99876c9196da4a5091f7a322edd07127
Melanie Gershman <melanie.gershman@gmail.com>
2016-11-25 20:05:22 -0500

    Add all question routes and begin view files, add errors partial

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,4 +1,43 @@
+# show all questions
 get '/questions' do
-  
+  @questions = Question.all
   erb :'questions/index'
+end
+
+# show specific question
+get '/questions/:id' do
+  @question = Question.find_by_id(params[:id])
+  erb :'questions/show'
+end
+
+# create a new question
+post '/questions' do
+  @question = Question.new(params[:question])
+
+  if @question.save
+    puts "question saved!"
+    redirect "/questions/#{@question.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :"_errors", layout: false, locals: {question: @question}
+  end
+end
+
+# form to get to edit a question
+get '/questions/:id/edit' do
+  @question = Question.find_by_id(params[:id])
+  erb :'questions/edit'
+end
+
+put '/questions/:id' do
+  @question = Question.find_by_id(params[:id])
+
+  @question.update_attributes(params[:question])
+
+  if @question.save
+    redirect "/questions/#{@question.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :"_errors", layout: false, locals: {question: @question}
+  end
 end
